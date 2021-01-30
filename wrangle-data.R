5afa634a75af8754748c751db0a23eded9241c1a
Ed Talanian <36826759+ETalanian@users.noreply.github.com>
2021-01-30 17:41:31 -0500

    C5: 1.1 Data Import, added paths and working dir

--- wrangle-data.R
+++ wrangle-data.R
@@ -2,3 +2,13 @@ library(tidyverse)
 murders <- read_csv('data/murders.csv')
 murders <- murders %>% mutate(region= factor(region), rate = total/population*10^5)
 save(murders, file= 'rda/murders.rda')
+
+
+
+system.file("extdata",package="dslabs")
+path <- system.file("extdata",package='dslabs')
+list.files(path)
+filename <- 'murders.csv'
+fullpath <- file.path(path, filename)
+fullpath
+file.copy(fullpath, getwd())
