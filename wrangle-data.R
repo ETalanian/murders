9ce5d3df5c9cfefcf139cee9feff114c755185e5
Ed Talanian <36826759+ETalanian@users.noreply.github.com>
2021-01-30 17:45:48 -0500

    C6* 1.1: Data Import The readr and readxl Packages

--- wrangle-data.R
+++ wrangle-data.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(readxl)
 murders <- read_csv('data/murders.csv')
 murders <- murders %>% mutate(region= factor(region), rate = total/population*10^5)
 save(murders, file= 'rda/murders.rda')
@@ -12,3 +13,7 @@ filename <- 'murders.csv'
 fullpath <- file.path(path, filename)
 fullpath
 file.copy(fullpath, getwd())
+file.exists(filename)
+dat <- read_csv(filename)
+dat <- read_csv(fullpath)
+head(dat)
