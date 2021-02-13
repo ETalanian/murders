a447159ec1d5b34d767580b918c521b5b86aa85d
Ed Talanian <36826759+ETalanian@users.noreply.github.com>
2021-02-13 18:01:11 -0500

    C6:2.3 Web Scraping

--- Tidy Data 2 Combining Tables.R
+++ Tidy Data 2 Combining Tables.R
@@ -85,3 +85,29 @@ setequal(1:5,1:6)
 setequal(1:5,5:1)
 setequal(tab1, tab2)
 
+#assessment
+install.packages("Lahman")
+library(Lahman)
+top <- Batting %>% 
+  filter(yearID == 2016) %>%
+  arrange(desc(HR)) %>%    # arrange by descending HR count
+  slice(1:10)    # take entries 1-10
+top %>% as_tibble()
+
+AwardsPlayers
+AwardsPlayers %>% filter(yearID=='2016')
+
+
+library(rvest) #rvest = harvest
+url <- "https://en.wikipedia.org/wiki/Murder_in_the_United_States_by_state"
+h <- read_html(url)
+class(h)
+h
+tab <- h %>% html_nodes('table')
+tab <- tab[[2]]
+tab
+#turn tab into a data frame
+tab <- tab %>% html_table
+class(tab)
+tab <- tab %>% setNames(c("state", "population", "total", "murders", "gun_murders", "gun_ownership", "total_rate", "murder_rate", "gun_murder_rate"))
+head(tab)
