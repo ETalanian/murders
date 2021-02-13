bc26897f08147e284ce7873b3a3c09940125e2f6
Ed Talanian <36826759+ETalanian@users.noreply.github.com>
2021-02-13 18:13:43 -0500

    C6:2.3 Assessment

--- Tidy Data 2 Combining Tables.R
+++ Tidy Data 2 Combining Tables.R
@@ -111,3 +111,24 @@ tab <- tab %>% html_table
 class(tab)
 tab <- tab %>% setNames(c("state", "population", "total", "murders", "gun_murders", "gun_ownership", "total_rate", "murder_rate", "gun_murder_rate"))
 head(tab)
+
+#assessment
+url <- "https://web.archive.org/web/20181024132313/http://www.stevetheump.com/Payrolls.htm"
+h <- read_html(url)
+nodes <- html_nodes(h, "table")
+html_text(nodes[[8]])
+html_table(nodes[[8]])
+html_table(nodes[[4]])
+tab_1 <- html_table(nodes[[10]])
+tab_2 <- html_table(nodes[[19]])
+full_join(tab_1,tab_2)
+
+url <- "https://en.wikipedia.org/w/index.php?title=Opinion_polling_for_the_United_Kingdom_European_Union_membership_referendum&oldid=896735054"
+h <- read_html(url)
+class(h)
+h
+tab <- h %>% html_nodes('table')
+tab <- read_html(url) %>% html_nodes('table') #same output
+tab
+length(tab)
+head(html_table(tab, fill=TRUE))
