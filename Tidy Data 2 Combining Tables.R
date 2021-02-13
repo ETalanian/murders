dfa2775bd643885c5d3f1fa4a3c844b47a0c4eb1
Ed Talanian <36826759+ETalanian@users.noreply.github.com>
2021-02-13 17:30:28 -0500

    C6:2.2 Binding

--- Tidy Data 2 Combining Tables.R
+++ Tidy Data 2 Combining Tables.R
@@ -46,3 +46,17 @@ semi_join(tab1, tab2)
 anti_join(tab1, tab2)
 #full join: Join data, return all values, all rows
 full_join(tab1,tab2)
+
+
+#Binding functions do not try to match by a variable, but rather just combine data sets
+bind_cols(a=1:3, b=4:6)
+#Bind can also bind data frames
+tab1 <- tab[,1:3]
+tab2 <- tab[,4:6]
+tab3 <- tab[,7:9]
+new_tab <- bind_cols(tab1,tab2,tab3)
+head(new_tab)
+
+tab1 <- tab[1:2,]
+tab2 <- tab[3:4,]
+bind_rows(tab1,tab2)
