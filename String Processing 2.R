a93b5a0a7254e3a723afd54f4bc1a519d3abaaec
Ed Talanian <36826759+ETalanian@users.noreply.github.com>
2021-02-27 19:16:59 -0500

    C6:3.2 Regex

--- String Processing 2.R
+++ String Processing 2.R
@@ -27,3 +27,41 @@ problems <- reported_heights %>%
   filter(not_inches(height)) %>%
   .$height
 length(problems)
+
+# 10 examples of x'y or x'y" or x'y\"
+pattern <- "^\\d\\s*'\\s*\\d{1,2}\\.*\\d*'*\"*$"
+str_subset(problems, pattern) %>% head(n=10) %>% cat
+
+# 10 examples of x.y or x,y
+pattern <- "^[4-6]\\s*[\\.|,]\\s*([0-9]|10|11)$"
+str_subset(problems, pattern) %>% head(n=10) %>% cat
+
+# 10 examples of entries in cm rather than inches
+ind <- which(between(suppressWarnings(as.numeric(problems))/2.54, 54, 81) )
+ind <- ind[!is.na(ind)]
+problems[ind] %>% head(n=10) %>% cat
+
+# detect whether a comma is present
+pattern <- ","
+str_detect(murders_raw$total, pattern) 
+
+#Find all entries with 'cm' in them
+str_subset(reported_heights$height, 'cm')
+
+# use the "or" symbol inside a regex (|)
+yes <- c("180 cm", "70 inches")
+no <- c("180", "70''")
+s <- c(yes, no)
+str_detect(s, "cm") | str_detect(s, "inches")
+str_detect(s, "cm|inches")
+
+#\d = digit (0,1,2,3,4,5,6,7,8,9)
+yes <- c('5','6',"5'10",'5 feet',"4'11")
+no <- c("",'.','Five','six')
+s <- c(yes,no)
+#use \\d so r doesn't just cancel the d
+pattern <- "\\d"
+str_detect(s,pattern)
+
+str_view(s,pattern)
+str_view_all(s,pattern)
