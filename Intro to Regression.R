c6be2916eeae52f2ad1b22042c68d7514f352a52
Ed Talanian <36826759+ETalanian@users.noreply.github.com>
2021-03-14 17:14:07 -0400

    C7:1.1 Bases on Balls or Stolen Bases

---
+++ Intro to Regression.R
@@ -0,0 +1,21 @@
+library(Lahman)
+library(tidyverse)
+library(dslabs)
+ds_theme_set()
+
+#HRs vs Wins
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_per_game = HR / G, R_per_game = R / G) %>%
+  ggplot(aes(HR_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+#SBs vs Wins
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(SB_per_game = SB / G, R_Per_Game = R/ G) %>%
+  ggplot(aes(SB_per_game, R_Per_Game)) +
+  geom_point(alpha = .5)
+#BBs vs Wins
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(BB_per_game = BB / G, R_per_game = R / G) %>%
+  ggplot(aes(BB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
