ddbcdf89abb0da4f3b7705d99f9d805ca3181fc3
Ed Talanian <36826759+ETalanian@users.noreply.github.com>
2021-03-14 17:19:59 -0400

    C7:1.1 Assessment: Baseball as a Motivating Example

--- Intro to Regression.R
+++ Intro to Regression.R
@@ -18,4 +18,18 @@ Teams %>% filter(yearID %in% 1961:2001) %>%
   mutate(BB_per_game = BB / G, R_per_game = R / G) %>%
   ggplot(aes(BB_per_game, R_per_game)) + 
   geom_point(alpha = 0.5)
-
+#ABs vs Wins
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(AB_per_game = AB / G, R_per_game = R / G) %>%
+  ggplot(aes(AB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+#Es vs Wins
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(E_per_game = E / G, R_per_game = R / G) %>%
+  ggplot(aes(E_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+#Triples vs Doubles
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(T_per_game = X3B / G, D_per_game = X2B / G) %>%
+  ggplot(aes(T_per_game, D_per_game)) + 
+  geom_point(alpha = 0.5)
