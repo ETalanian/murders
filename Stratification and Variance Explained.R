dcc4dd37b1dabce27d6e04277fb28a9d2fe4159b
Ed Talanian <36826759+ETalanian@users.noreply.github.com>
2021-03-19 17:41:29 -0400

    C7:1.3 Assessment Scratchpad

--- Stratification and Variance Explained.R
+++ Stratification and Variance Explained.R
@@ -53,3 +53,31 @@ galton_heights %>%
   stat_qq(aes(sample=son)) + 
   facet_wrap(~z_father)
 
+#Assessment Scratchpad
+set.seed(1989, sample.kind="Rounding") #if you are using R 3.6 or later
+library(HistData)
+data("GaltonFamilies")
+
+female_heights <- GaltonFamilies%>%     
+  filter(gender == "female") %>%     
+  group_by(family) %>%     
+  sample_n(1) %>%     
+  ungroup() %>%     
+  select(mother, childHeight) %>%     
+  rename(daughter = childHeight)
+
+head(female_heights)
+mu_x <- mean(female_heights$mother)
+mu_x
+mu_y <- mean(female_heights$daughter)
+mu_y
+s_x <- sd(female_heights$mother)
+s_x
+s_y <- sd(female_heights$daughter)
+s_y
+r <- cor(female_heights$mother, female_heights$daughter)
+r
+m <- r * s_y/s_x
+m
+b <- mu_y - m*mu_x
+b
