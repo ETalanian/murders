9e807e25eee66ff2168743e44d80412c464c8a7b
Ed Talanian <36826759+ETalanian@users.noreply.github.com>
2021-03-21 19:10:25 -0400

    C7:2.1 Stratification and Multivariate Regression

--- Linear Models.R
+++ Linear Models.R
@@ -22,3 +22,38 @@ Teams %>%
   mutate(Singles = (H-HR-X2B-X3B)/G, BB=BB/G, HR=HR/G) %>%
   summarize(cor(BB,HR), cor(Singles,HR), cor(BB,Singles))
 
+#stratify home runs per game to the closest 10th, filter out strata with few points
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_strata = round(HR/G,1),
+         BB_per_game = BB/G,
+         R_per_game = R/G) %>%
+  filter(HR_strata >= .4 & HR_strata <=1.2)
+dat
+#scatterplot for each HR stratum
+dat %>%
+  ggplot(aes(BB_per_game, R_per_game)) +
+  geom_point(alpha=.5)+
+  geom_smooth(method = 'lm')+
+  facet_wrap(~HR_strata)
+#calculate slope of regression line after stratifying by HR
+dat %>%
+  group_by(HR_strata) %>%
+  summarize(slope = cor(BB_per_game, R_per_game)*sd(R_per_game)/sd(BB_per_game))
+
+#stratify by BB
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(BB_strata = round(BB/G, 1), 
+         HR_per_game = HR / G,
+         R_per_game = R / G) %>%
+  filter(BB_strata >= 2.8 & BB_strata <=3.9) 
+
+#scatterplot for each BB stratum
+dat %>% ggplot(aes(HR_per_game, R_per_game)) +  
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm") +
+  facet_wrap( ~ BB_strata)
+
+#slope of regression line after stratifying by BB
+dat %>%  
+  group_by(BB_strata) %>%
+  summarize(slope = cor(HR_per_game, R_per_game)*sd(R_per_game)/sd(HR_per_game)) 
