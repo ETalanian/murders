fb0101de4a183b522deb7a05fcd6e0da6463db07
Ed Talanian <36826759+ETalanian@users.noreply.github.com>
2021-06-08 18:01:46 -0400

    C8:1.1 Intro to Machine Learning

---
+++ Intro to Machine Learning.R
@@ -0,0 +1,12 @@
+#Y = Outcomes
+#X's (X1 - Xp) = Features (or predictors, or covariates)
+
+#Prediction problems can be Categorical or Continuous
+#For Categorical outcomes, Y can be any of k classes
+#ie k in a digit reader = 10 (1,2,3,4,5,6,7,8,9,0)
+#ie k in speach detection = All posisble words
+#ie k in a spam filter = 2 (Spam | Not Spam)
+#in this course, k = 1 through K, and binary k = 0,1
+#When the outcome is categorical, we refer to the machine-learning task as classification.
+#our predictions will be categorical, just like our outcomes, and they will either be correct or incorrect.
+#when the outcome is continuous, we'll refer to the task as prediction, which will be measured in error, instead of right/wrong.
